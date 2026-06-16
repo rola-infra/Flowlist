cb51bc7374415b93944ee6f2eba9d279353c491f
rola-infra <rolaregex@gmail..com>
2026-06-16 18:51:04 +0530

    global error handling implementation done

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import taskRoutes from './routes/task.routes.js';
 import authRoutes from './routes/auth.routes.js';
+import { globalErrorHandler } from './controllers/error.controller.js';
 
 const app = express();
 
@@ -8,5 +9,7 @@ app.use(express.json());
 
 app.use('/api/v1/tasks', taskRoutes);
 app.use('/api/v1/auth', authRoutes);
+
+app.use(globalErrorHandler);
 
 export default app;
--- Server/src/controllers/task.controller.js
+++ Server/src/controllers/task.controller.js
@@ -1,128 +1,84 @@
 import Task from '../models/task.model.js';
-
+import AppError from '../utils/appError.js';
 export const createTask = async (req, res) => {
-  try {
-    const task = await Task.create({ ...req.body, userId: req.user.id });
-    res.status(201).json({
-      status: 'success',
-      data: {
-        task,
-      },
-    });
-  } catch (error) {
-    console.log(error.message);
-
-    res.status(500).json({
-      status: 'fail',
-      message: error.message,
-    });
-  }
+  const task = await Task.create({ ...req.body, userId: req.user.id });
+  res.status(201).json({
+    status: 'success',
+    data: {
+      task,
+    },
+  });
 };
 
 export const getTasks = async (req, res) => {
-  try {
-    const tasks = await Task.find({
-      userId: req.user.id,
-    });
+  const tasks = await Task.find({
+    userId: req.user.id,
+  });
 
-    res.status(200).json({
-      status: 'success',
-      results: tasks.length,
-      data: {
-        tasks,
-      },
-    });
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({
-      status: 'fail',
-      message: error.message,
-    });
-  }
+  res.status(200).json({
+    status: 'success',
+    results: tasks.length,
+    data: {
+      tasks,
+    },
+  });
 };
 
-export const getTaskById = async (req, res) => {
-  try {
-    const task = await Task.findOne({
-      _id: req.params.id,
-      userId: req.user.id,
-    });
+export const getTaskById = async (req, res, next) => {
+  const task = await Task.findOne({
+    _id: req.params.id,
+    userId: req.user.id,
+  });
 
-    if (!task) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'Task not found',
-      });
-    }
-    res.status(200).json({
-      status: 'success',
-      data: {
-        task,
-      },
-    });
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({
-      status: 'fail',
-      message: error.message,
-    });
+  if (!task) {
+    return next(new AppError('task not found', 404));
   }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      task,
+    },
+  });
 };
 
 export const updateTask = async (req, res) => {
-  try {
-    const task = await Task.findOneAndUpdate(
-      {
-        _id: req.params.id,
-        userId: req.user.id,
-      },
-      req.body,
-      {
-        new: true,
-        runValidators: true,
-      },
-    );
+  const task = await Task.findOneAndUpdate(
+    {
+      _id: req.params.id,
+      userId: req.user.id,
+    },
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    },
+  );
 
-    if (!task) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'Task not found',
-      });
-    }
-    res.status(200).json({
-      status: 'success',
-      data: {
-        task,
-      },
-    });
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({
+  if (!task) {
+    return res.status(404).json({
       status: 'fail',
-      message: error.message,
+      message: 'Task not found',
     });
   }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      task,
+    },
+  });
 };
 
 export const deleteTask = async (req, res) => {
-  try {
-    const task = await Task.findOneAndDelete({
-      _id: req.params.id,
-      userId: req.user.id,
-    });
+  const task = await Task.findOneAndDelete({
+    _id: req.params.id,
+    userId: req.user.id,
+  });
 
-    if (!task) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'Task not found',
-      });
-    }
-    res.status(204).send();
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({
+  if (!task) {
+    return res.status(404).json({
       status: 'fail',
-      message: error.message,
+      message: 'Task not found',
     });
   }
+  res.status(204).send();
 };
